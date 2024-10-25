afb7b049e7d96b8c72f56527fad2ace905eb594b
Harsh Deshmukh <deshmukhharsh2507@gmail.com>
2024-10-25 18:03:25 +0530

    Create navigation Buttons

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import DisplayHome from "./DisplayHome";
 import DisplayAlbum from "./DisplayAlbum";
+import { albumsData } from "../assets/assets";
 
 const Display = () => {
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const bgColor = albumsData[Number(albumId)].bgColor;
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`;
+    } else {
+      displayRef.current.style.background = `#121212`;
+    }
+  });
+
   return (
-    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0 ">
+    <div
+      ref={displayRef}
+      className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0 "
+    >
       <Routes>
         <Route path="/" element={<DisplayHome />} />
         <Route path="/album/:id" element={<DisplayAlbum />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibond">
         <div className="flex items-center gap-2">
           <img
+            onClick={() => navigate(-1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_left}
             alt=""
           />
           <img
+            onClick={() => navigate(1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_right}
             alt=""
