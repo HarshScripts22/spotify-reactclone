756363c1dbe36b8944cb8742677436153a7e1209
Harsh Deshmukh <146616415+HarshScripts22@users.noreply.github.com>
2024-12-08 18:51:32 +0530

    Update ChartsCard.jsx

--- src/components/ChartsCard.jsx
+++ src/components/ChartsCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PlayBtn from "./playBtn";
+import PlayBtn from "./PlayBtn";
 
 const ChartsCard = ({ chartImg, chartDesc }) => {
   return (
